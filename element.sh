57637a118e4a6dd85ce9ec9dc6a7beca790819e4
Gabriel Garro <ggarro_1303@hotmail.com>
2024-06-14 04:18:13 +0000

    fix: argument request when nothing is passed

--- element.sh
+++ element.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-echo "Please provide an element as an argument."
-echo "$1"
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else  
+  echo "$1"
+fi
 
 if [[ $1 =~ [0-9] ]];
 then
